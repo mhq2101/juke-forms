21808aea8689da39b9d02c9a23ffd98b34371965
Mikhail Qader <mhq2101@columbia.edu>
2017-07-06 17:43:14 -0400

    filter working

--- browser/react/components/AllArtists.js
+++ browser/react/components/AllArtists.js
@@ -4,32 +4,52 @@ import axios from 'axios';
 
 export default class AllArtists extends Component {
 
-  constructor () {
+  constructor() {
     super();
     this.state = {
-      artists: []
+      artists: [],
+      searchArtist: ''
     };
+    this.handleChange = this.handleChange.bind(this);
+
   }
 
-  componentDidMount () {
+  handleChange(event) {
+    console.log(this.state)
+
+    this.setState({
+      searchArtist: event.target.value,
+    })
+
+  }
+
+  componentDidMount() {
     axios.get('/api/artists')
       .then(res => res.data)
       .then(artists => this.setState({ artists }));
   }
 
-  render () {
+  render() {
 
-    const artists = this.state.artists;
+    const artists = this.state.artists.filter(artist => artist.name.match(this.state.searchArtist));
 
     return (
       <div>
+        <form className="form-group" style={{ marginTop: '20px' }}>
+          <input
+            className="form-control"
+            placeholder="Enter artist name"
+            onChange={this.handleChange}
+          />
+        </form>
+
         <h3>Artists</h3>
         <div className="list-group">
           {
             artists.map(artist => {
               return (
                 <div className="list-group-item" key={artist.id}>
-                  <Link to={`/artists/${artist.id}`}>{ artist.name }</Link>
+                  <Link to={`/artists/${artist.id}`}>{artist.name}</Link>
                 </div>
               );
             })
